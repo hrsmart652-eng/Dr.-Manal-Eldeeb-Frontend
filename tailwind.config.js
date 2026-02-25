e7da5eb1a3a126bd7ec8fd14921a8caa5c1a58db
menna gamal <mennag093@gmail.com>
2026-02-25 05:09:29 +0200

    fix: re-organize folder structure and move package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
-     fontFamily: {
-        'Tajawal': ['Tajawal', 'sans-serif'],
-        'reem-kufi': ['"Reem Kufi Fun"', 'sans-serif'],
-      },
   },
   plugins: [],
 }
-
